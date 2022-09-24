31b9b204de504b85eeae09522335e7ce8e28f77f
Kulakov D.A., ISTb-21-1 <dragonipam@yandex.ru>
2022-09-24 11:40:33 +0800

    Added gui programs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.penny = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // penny
+            // 
+            this.penny.Location = new System.Drawing.Point(12, 12);
+            this.penny.Name = "penny";
+            this.penny.Size = new System.Drawing.Size(173, 27);
+            this.penny.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(191, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите количество копеек";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(344, 78);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.penny);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Валютный калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox penny;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,15 @@ namespace task1_gui
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
